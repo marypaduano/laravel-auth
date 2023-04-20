33d445a5ba67fb914b36e72c4c2ff9566e42879d
marypaduano <maria.paduano@gmail.com>
2023-04-20 20:09:27 +0200

    FEAT index table

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/projects/create', [ProjectController::class, 'create'])->name('projects.create');
+
+Route::get('/projects/{project}', [ProjectController::class, 'show'])->name('projects.show');
+
+Route::post('/projects', [ProjectController::class, 'store'])->name('projects.store');
+
+Route::get('/projects/{project}/edit', [ProjectController::class, 'edit'])->name('projects.edit');
+
+Route::put('/projects/{project}', [ProjectController::class, 'update'])->name('projects.update');
+
+Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
